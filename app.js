5b926ebeccf3e1df092cf1fffed0db3860e3b245
Rafael Reales <rafitha.19960607@gmail.com>
2023-05-24 11:01:31 -0500

    fist

---
+++ app.js
@@ -0,0 +1,40 @@
+// Definir la lista de contactos
+let listaDeContactos = [
+  "Juan Perez",
+  "Maria Lopez",
+  "Pedro Gonzalez",
+  "Laura Ramirez"
+];
+// console.log (listaDeContactos);
+
+// Función para añadir un nuevo contacto a la lista
+function agregarContacto(nombreApellido) {
+  listaDeContactos.push(nombreApellido);
+};
+
+// Función para borrar un contacto existente de la lista
+function borrarContacto(nombreApellido) {
+  let indice = listaDeContactos.indexOf(nombreApellido);
+  if (indice !== -1) {
+    listaDeContactos.splice(indice, 1);
+  }
+};
+
+// Función para imprimir en consola los contactos presentes en la lista
+function imprimirContactos() {
+  for (var i = 0; i < listaDeContactos.length; i++) {
+    console.log(listaDeContactos[i]);
+  }
+};
+console.log("Contactos actuales:");
+// imprimirContactos();
+
+console.log("\nAgregando nuevo contacto...");
+agregarContacto("Ana Martinez");
+console.log("Contactos después de agregar un nuevo contacto:");
+imprimirContactos();
+
+console.log("\nBorrando contacto existente...");
+borrarContacto("Pedro Gonzalez");
+console.log("Contactos después de borrar un contacto:");
+imprimirContactos();
